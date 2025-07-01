655486ce4914350d7f560da4ac26e963fda31cef
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2025-07-02 00:45:59 +0800

    Add UK and HK locales, update locale file names

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,21 +3,16 @@ import type { DefaultLocaleMessageSchema } from "vue-i18n";
 import { match } from "@formatjs/intl-localematcher";
 
 const locales = import.meta.glob("./*.json", { import: "default" });
-console.log("locales: ", locales);
 
 const supportedLocales = Object.keys(locales).map((key) => {
   return key.slice(2, -5); // Remove './' and '.json'
 });
-console.log("supportedLocales: ", supportedLocales);
-let matchResult = match(navigator.languages, supportedLocales, "en");
-console.log("matchResult: ", matchResult);
-import.meta.env.DEV && (matchResult = "zh-hant");
+let matchResult = match(navigator.languages, supportedLocales, "en-US");
 
 const messages: Record<string, DefaultLocaleMessageSchema> = {};
 messages[matchResult] = (await locales[
   `./${matchResult}.json`
 ]()) as DefaultLocaleMessageSchema;
-console.log("messages: ", messages);
 
 const i18n = createI18n({
   legacy: false,
@@ -28,4 +23,4 @@ const i18n = createI18n({
 const t = i18n.global.t;
 
 export default i18n;
-export { t };
+export { t, matchResult as currentLocale };
